d123b475b5b9be942d5a910105cddc19c9e6318f
Byron <youngkillian0308@gmail.com>
2025-02-17 16:12:47 +0200

    20th Commit - Fixing skill progress bar

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ window.onscroll = () => {
 					.querySelector(`header nav a[href*="${id}"]`)
 					.classList.add("active");
 			});
-			// active sections for animation on scroll
+
 			sec.classList.add("show-animate");
 		} else {
 			sec.classList.remove("show-animate");
@@ -36,9 +36,6 @@ window.onscroll = () => {
 	// Sticky header
 	let header = document.querySelector("header");
 	header.classList.toggle("sticky", window.scrollY > 100);
-
-	// Remove toggle icon and navbar when click navbar links (scroll)
-
 	menuIcon.classList.remove("bx-x");
 	navbar.classList.remove("active");
 
